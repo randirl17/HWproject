14d759c274a1cf74f97e66a5a22acec7d089fa89
Randi <randirl17@gmail.com>
2015-11-20 18:42:23 -0600

    establish HWpull.py

---
+++ HWpull.py
@@ -0,0 +1,19 @@
+#program to pull HW info from various websites and consolidate that to a single webpage
+
+import os
+import re
+import sys
+import urllib
+
+def routine():
+
+return
+
+
+
+def main():
+    img_urls = routine()
+
+
+if __name__ == '__main__':
+  main()
