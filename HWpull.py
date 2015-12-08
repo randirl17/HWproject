24f30dd7baa4eed2e816d4e616f46baa0104723c
Randi <randirl17@gmail.com>
2015-12-08 16:27:35 -0600

    pull html files from various websites

--- HWpull.py
+++ HWpull.py
@@ -5,14 +5,51 @@ import re
 import sys
 import urllib
 
-def routine():
+def urlget(url,endfile):
+  try:
+    ufile = urllib.urlopen(url)
+    if ufile.info().gettype() == 'text/html':
+        text=ufile.read()
+        h=open(endfile,'w')
+        h.write(text)
+        h.close()
+  except IOError:
+    print 'problem reading url:', link
 
-return
+def Jap(file):
+  j=open(file,'r')
+  jtxt=j.readlines()
+  j.close()
+  for line in jtxt:
+    if ">-Homework" in line:
+        return line
+  return
+
+def HWscript(filename):
+  dirname='./sitefiles'
+  if not os.path.exists(dirname):  os.mkdir(dirname)
+  f=open(filename,'r') #open and read sites.txt
+  for line in f:
+    urlparts=line.split(":")
+    classfile=urlparts[0].replace(' ','')+'.txt'
+    site=urlparts[2]
+    link=urlparts[1]+":"+site[:-1]
+    dest_name=os.path.join(dir,classfile)
+    urlget(link,dest_name)  #write html to txt files
+    if "Jap" in urlparts[0]:  JapHW=Jap(dest_name)
+  f.close()
+#  z=open('HWlist.txt','a')
+  
+
+
+
+    
+  return
 
 
 
 def main():
-    img_urls = routine()
+    img_urls = HWscript(sys.argv[1])
 
 
 if __name__ == '__main__':
