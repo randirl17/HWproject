cdd91a090b2ec18ce3cf1ab0a4ac891547e7946b
Randi <randirl17@gmail.com>
2015-12-13 16:44:17 -0600

    finds Math HW

--- HWpull.py
+++ HWpull.py
@@ -1,15 +1,18 @@
-#program to pull HW info from various websites and consolidate that to a single webpage
+"""
+Takes sites.txt list of websites, determines class name, downloads html and finds most recent assignment for each course.  Then writes these assignments to a single html file.
+"""
 
 import os
 import re
 import sys
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
 def urlget(url,endfile):
   try:
-    ufile = urllib.urlopen(url)
-    if ufile.info().gettype() == 'text/html':
+      ufile = urllib.request.urlopen(url)
+      print(ufile.info())
+      if ufile.info() == 'text/html':
         text=ufile.read()
         h=open(endfile,'w')
         h.write(text)
@@ -24,24 +27,35 @@ def Jap(file):
   match=re.findall(r'">([-\w\d\s,/]+)</font>',str(HWmatch[0]))
   return match
   
-
+def Math(file):
+  soup=BeautifulSoup(open(file))
+  title=soup.find(class_="announcement").h4.string
+  a=soup.find(class_="announcement").div.contents
+  newa=[]
+  for i in range(len(a)):
+    newa.append(str(a[i]).strip("</div>"))
+  return title+str(newa)
+  
 
 
 def HWscript(filename):
   dirname='./sitefiles'
   if not os.path.exists(dirname):  os.mkdir(dirname)
-  f=open(filename,'r') #open and read sites.txt
+  f=open(filename,'r') #open and read sites.txt list
   for line in f:
     urlparts=line.split(":")
     classfile=urlparts[0].replace(' ','')+'.txt'
     site=urlparts[2]
     link=urlparts[1]+":"+site[:-1]
-    dest_name=os.path.join(dir,classfile)
+    dest_name=os.path.join(dirname,classfile)
     urlget(link,dest_name)  #write html to txt files
     if "Jap" in urlparts[0]:  JapHW=Jap(dest_name)
+    if "Math" in urlparts[0]:  MathHW=Math(dest_name)
   f.close()
 
-  
+  print(JapHW)
+  print()
+  print(MathHW)  
 
 
 
